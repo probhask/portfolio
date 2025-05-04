5fd092912a251892044530de87ff729b863acdbc
Bhaskar Sharma <bhaskarneeraj23@gmail.com>
2025-05-04 12:25:56 +0530

    UPDATE: remove header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import AboutMe from "@/components/AboutMe";
 import ContactMe from "@/components/ContactMe";
 import Footer from "@/components/Footer";
-import Header from "@/components/Header";
+// import Header from "@/components/Header";
 import Intoduction from "@/components/Intoduction";
 import Projects from "@/components/Projects";
 import Skills from "@/components/Skills";
@@ -10,8 +10,8 @@ import { ThemeProvider } from "@/context/theme.context";
 export default function Home() {
   return (
     <ThemeProvider>
-      <div className="min-h-screen w-full container mx-auto bg-background px-2">
-        <Header />
+      <div className="min-h-screen   mx-auto  px-2 bg-background text-background">
+        {/* <Header /> */}
         <main className="space-y-8">
           <Intoduction />
           <AboutMe />
